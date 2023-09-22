ba417dc71f8d9377fa1d1bad9db96170d4510937
BlckTitan <dorshman406@gmail.com>
2023-09-22 15:48:05 +0100

    Learnt about http requests with focus on get request using expressJS Built my first expressJs server

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+const COURSES = [
+    {id: 1, course: 'Maths'},
+    {id: 2, course: 'English'},
+    {id: 3, course: 'Biology'}
+]
+
 app.get('/', (req, res ) => {
     res.send('Hello World')
 })
@@ -8,6 +15,27 @@ app.get('/', (req, res ) => {
 app.get('/again', (req, res) => {
     res.send([1, 2, 3, 4, 5])
 })
+
+app.get('/blog/:month/:year', (req, res) => {
+    res.send(req.params)
+})
+
+app.get('/posts/:month/:year', (req, res) => {
+    res.send(req.query)
+})
+
+app.get('/courses', (req, res) => {
+    res.send(COURSES)
+})
+app.get('/courses/:id', (req, res) => {
+    const COURSE_MATCH = COURSES.find((foundCourse) => foundCourse.id === parseInt(req.params.id));
+    if(!COURSE_MATCH){
+        res.status(404).send('The course with the given ID was not found...')
+    }else{
+        res.send(COURSE_MATCH)
+    }
+})
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
