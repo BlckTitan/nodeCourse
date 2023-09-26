c713d3df3a08ba186dbaf22802e44e5a2e2a2c67
BlckTitan <dorshman406@gmail.com>
2023-09-26 14:49:46 +0100

    Lesson on http request validation using Joi

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi')
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 
 const COURSES = [
@@ -8,32 +11,64 @@ const COURSES = [
     {id: 3, course: 'Biology'}
 ]
 
+
+//sending http requests
+
 app.get('/', (req, res ) => {
     res.send('Hello World')
 })
 
-app.get('/again', (req, res) => {
+app.get('/api/again', (req, res) => {
     res.send([1, 2, 3, 4, 5])
 })
 
-app.get('/blog/:month/:year', (req, res) => {
+app.get('/api/blog/:month/:year', (req, res) => {
     res.send(req.params)
 })
 
-app.get('/posts/:month/:year', (req, res) => {
+app.get('/api/posts/:month/:year', (req, res) => {
     res.send(req.query)
 })
 
-app.get('/courses', (req, res) => {
+app.get('/api/courses', (req, res) => {
     res.send(COURSES)
 })
-app.get('/courses/:id', (req, res) => {
+
+//handling gete requests
+
+app.get('/api/courses/:id', (req, res) => {
     const COURSE_MATCH = COURSES.find((foundCourse) => foundCourse.id === parseInt(req.params.id));
     if(!COURSE_MATCH){
         res.status(404).send('The course with the given ID was not found...')
     }else{
         res.send(COURSE_MATCH)
     }
+})
+
+//post request
+app.post('/api/courses', (req, res) => {
+
+    //validating requests using Joi
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+
+    const RESULT = schema.validate(req.body)
+    
+
+    if(RESULT.error){
+        //400 Bad request
+        res.status(400).send(RESULT.error.details[0].message);
+        return
+    }
+
+    const COURSE = {
+        id: COURSES.length + 1,
+        name: req.body.name
+    }
+
+    COURSES.push(COURSE);
+    res.send(COURSE);
 })
 
 const port = process.env.PORT || 5000;
