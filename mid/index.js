fa05d614233c02920385209d180f3ccde6062200
BlckTitan <dorshman406@gmail.com>
2023-10-02 13:42:55 +0100

    Lesson on working with third party middleware

--- mid/index.js
+++ mid/index.js
@@ -1,3 +1,5 @@
+const morgan = require('morgan')
+const helmet = require('helmet')
 const logger = require('./logger')
 const auth = require('./auth');
 const express = require('express')
@@ -8,9 +10,24 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(auth);
 app.use(logger);
+app.use(morgan('tiny'));
+app.use(helmet());
+
+const GENRES = [
+    {id: 1, name: "Horror"},
+    {id: 2, name: "Comedy"},
+    {id: 3, name: "Action"},
+    {id: 4, name: "Romance"},
+    {id: 5, name: "Sci-Fi"},
+    {id: 6, name: "Drama"}
+]
 
 app.get('/', (req, res) => {
     console.log('serving...')
+})
+
+app.get('/api/mid', (req, res) => {
+    res.send(GENRES)
 })
 
 const PORT = process.env.PORT || 3000;
